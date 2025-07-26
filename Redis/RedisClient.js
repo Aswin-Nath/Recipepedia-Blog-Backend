32627c1a52c36ef7019f3e658940d719933b4809
Aswin-Nath <aswinnathte125@gmail.com>
2025-07-26 13:19:21 +0530

    Redis Client Changes

--- Redis/RedisClient.js
+++ Redis/RedisClient.js
@@ -1,18 +1,16 @@
-// redisClient.js
-import { createClient } from 'redis';
-import dotenv from 'dotenv';
-dotenv.config();
+const { createClient } = require('redis');
+require('dotenv').config();
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
   socket: {
-    tls: true, // Required for Upstash
+    tls: true
   },
-  legacyMode: true, // Optional, useful if you use .get/.set the old way
+  legacyMode: true
 });
 
 redisClient.connect()
   .then(() => console.log('✅ Connected to Upstash Redis'))
   .catch(err => console.error('❌ Redis connection error:', err));
 
-export default redisClient;
+module.exports = redisClient;
